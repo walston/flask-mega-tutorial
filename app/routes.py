06488481a8d5f99977ff9cb01404404936eb983a
Nathan Walston <walston@users.noreply.github.com>
2024-02-12 04:14:57 -0800

    generating links

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import flash, render_template, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -28,5 +28,5 @@ def login():
             'Login requested for user {}, remember_me={}'
             .format(form.username.data, form.remember_me.data)
         )
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
