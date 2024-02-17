e5c1580e58854da5a21deb4e6a634389a0917be5
Nathan Walston <walston@users.noreply.github.com>
2024-02-17 07:08:31 -0800

    removes mock user

--- app/routes.py
+++ app/routes.py
@@ -11,12 +11,11 @@ from urllib.parse import urlsplit
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Miguel'}
     posts = [
         {'author': {'username': 'John'},  'body': 'Beautiful day in Portland'},
         {'author': {'username': 'Susan'}, 'body': 'The Avengers movie was so cool!'}
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
