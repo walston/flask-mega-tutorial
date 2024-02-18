6443f6875244e133ace5abc4097de85d4bb190fa
Nathan Walston <walston@users.noreply.github.com>
2024-02-17 16:45:20 -0800

    Create a user profile page for current user

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from urllib.parse import urlsplit
+from werkzeug.exceptions import NotFound
 
 
 @app.route('/')
@@ -58,3 +59,14 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
