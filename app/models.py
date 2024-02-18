ba61f63722778bb0e9f1e27006a37586a8d2d25b
Nathan Walston <walston@users.noreply.github.com>
2024-02-18 08:10:50 -0800

    Chapter 5 (#5)
    [Chapter 5: User
    Logins](https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-v-user-logins)
    
    1. Password Hashing
    2. Introduction to Flask-Login
    3. Preparing The User Model for Flask-Login
    4. User Loader Function
    5. Logging Users In
    6. Logging Users Out
    7. Requiring Users To Login
    8. Showing The Logged-In User in Templates
    9. User Registration

--- app/models.py
+++ app/models.py
@@ -1,11 +1,18 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = \
         so.mapped_column(primary_key=True)
     username: so.Mapped[str] = \
@@ -16,6 +23,12 @@ class User(db.Model):
         so.mapped_column(sa.String(256))
     posts: so.WriteOnlyMapped['Post'] = \
         so.relationship(back_populates='author')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
