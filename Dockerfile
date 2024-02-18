2fbb078dc4f52ec989d967eb98b40b9cb20df407
Nathan Walston <walston@users.noreply.github.com>
2024-02-18 08:00:35 -0800

    Chapter 4 (#4)
    [Chapter 4:
    Database](https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database)
    
    1. Databases in Flask
    2. Database Migrations
    3. Flask-SQLAlchemy Configuration
    4. Database Models
    5. Creating The Migration Repository
    6. The First Database Migration
    7. Database Upgrade and Downgrade Workflow
    8. Database Relationships
    9. Playing with the Database
    10. Shell Context

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM python:3.12-slim-bookworm
 WORKDIR /usr/src
 COPY ./requirements.txt /usr/src
+ENV FLASK_APP="/usr/src/microblog.py"
+ENV SQLALCHEMY_DATABASE_URI="/usr/src/app.db"
 RUN ["pip", "install", "-r", "requirements.txt"]
-ENV FLASK_APP=microblog.py
 EXPOSE 8080
 CMD ["flask", "--app", "run", "--host", "0.0.0.0", "--port", "8080"]
--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+Migrate = Migrate(app, db)
 
 # fmt: off # circular import if above `app` declaration
-from app import routes
+from app import routes, models
