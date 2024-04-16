2911019aebb3f1f3160646a92f11d7fb087780cb
Nathan Walston <walston@users.noreply.github.com>
2024-04-15 20:03:24 -0700

    Submission of Blog Posts & Displaying Blog Posts

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,8 @@ import sqlalchemy as sa
 from flask import flash, render_template, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
-from app.forms import EditProfileForm, LoginForm, RegistrationForm, EmptyForm
-from app.models import User
+from app.forms import EditProfileForm, LoginForm, PostForm, RegistrationForm, EmptyForm
+from app.models import Post, User
 from urllib.parse import urlsplit
 from datetime import datetime, timezone
 
@@ -15,15 +15,19 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {'author': {'username': 'John'},  'body': 'Beautiful day in Portland'},
-        {'author': {'username': 'Susan'}, 'body': 'The Avengers movie was so cool!'}
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = db.session.scalars(current_user.following_posts()).all()
+    return render_template('index.html', title='Home', posts=posts, form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
