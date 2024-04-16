209493a1bd993afbf4222a8325bc11eb1ec61a48
Nathan Walston <walston@users.noreply.github.com>
2024-04-15 21:14:51 -0700

    Pagination in the User Profile Page

--- app/routes.py
+++ app/routes.py
@@ -35,7 +35,8 @@ def index():
         if posts.has_next else None
     prev_url = url_for('index', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('index.html', title='Home', posts=posts, form=form, next_url=next_url, prev_url=prev_url)
+    return render_template('index.html', title='Home', posts=posts, form=form,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/explore')
@@ -51,7 +52,8 @@ def explore():
         if posts.has_next else None
     prev_url = url_for('explore', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('index.html', title='Explore', posts=posts, next_url=next_url, prev_url=prev_url)
+    return render_template('index.html', title='Explore', posts=posts,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -100,12 +102,19 @@ def register():
 @login_required
 def user(username):
     user = db.first_or_404(sa.select(User).where(User.username == username))
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    page = request.args.get('page', 1, type=int)
+    query = user.posts.select().order_by(Post.timestamp.desc())
+    posts = db.paginate(query,
+                        page=page,
+                        per_page=app.config['POSTS_PER_PAGE'],
+                        error_out=False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, posts=posts, form=form,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
