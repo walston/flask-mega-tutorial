9911830118867fc4dcf6e5bad3b9a05b11f08003
Nathan Walston <walston@users.noreply.github.com>
2024-04-16 16:49:12 -0700

    Introduction to Flask-Babel

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from logging.handlers import RotatingFileHandler
+from flask_moment import Moment
+from flask_babel import Babel
+
 import logging
 import os
+
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +21,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
 
 # fmt: off # circular import if above `app` declaration
 from app import routes, models, errors
