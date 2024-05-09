7ef1fb5023cc9f04cdaaa1c88d9357f954c59af6
Nathan Walston <walston@users.noreply.github.com>
2024-05-08 19:39:15 -0700

    Ajax From The Server

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,7 @@ from urllib.parse import urlsplit
 from datetime import datetime, timezone
 from flask_babel import _, get_locale
 from langdetect import detect, LangDetectException
+from app.translate import translate
 
 
 @app.before_request
@@ -184,3 +185,12 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    data = request.get_json()
+    return {'text': translate(data['text'],
+                              data['source_language'],
+                              data['dest_language'])}
