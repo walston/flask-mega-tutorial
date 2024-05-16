d8845483761b53f05025c7abb87fef585d896cc1
Nathan Walston <walston@users.noreply.github.com>
2024-05-15 17:02:44 -0700

    add some shell context
    for fun and profit

--- microblog.py
+++ microblog.py
@@ -1,2 +1,9 @@
-from app import app
-from app import cli
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db, cli
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
