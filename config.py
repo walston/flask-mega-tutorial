c875d9501d9aaad249423b234aca6e967303e8c1
Nathan Walston <walston@users.noreply.github.com>
2024-09-09 18:48:50 -0700

    gets app running based on docker-compose

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    Debug = os.environ.get('FLASK_DEBUG') or False
+    DEBUG = os.environ.get('FLASK_DEBUG') or False
+    HOST = os.environ.get('FLASK_HOST') or '0.0.0.0'
+    PORT = os.environ.get('FLASK_PORT') or 5000
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
